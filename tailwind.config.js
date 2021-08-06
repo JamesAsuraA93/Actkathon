cccf05ab23b0e1caa876925e188a7461c222d9cb
matcha <meen.sankaew@gmail.com>
2021-08-06 14:29:53 +0700

    fix: add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
-const plugin = require('tailwindcss/plugin')
-
-module.exports = {
-  plugins: [
-    plugin(function({ addBase, theme }) {
-      addBase({
-        'h1': { fontSize: theme('fontSize.2xl') },
-        'h2': { fontSize: theme('fontSize.xl') },
-        'h3': { fontSize: theme('fontSize.lg') },
-      })
-    })
-  ]
-}
+};
