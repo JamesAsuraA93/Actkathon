16cc50fc336d900913560d334f855ae4c80b19ab
JamesAsuraA93 <AsuraA93@gmail.com>
2021-08-06 13:20:43 +0700

    add bug in this project

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,27 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import logo  from '../public/icon/logo.svg' 
+import SvgComponent from '../component/Pre-home'
+import { useEffect, useRef } from 'react'
+
 
 export default function Home() {
+  const ref = useRef<HTMLDivElement>(null)
+  const mask = useRef<HTMLDivElement>(null)
+  useEffect(()=>{
+    ref.current?.addEventListener("mousemove",(event)=>{
+      event.preventDefault();
+      console.log(event)
+      var upX = event.clientX;
+      var upY = event.clientY;
+      // var mask = ('#mask1 circle')[0];
+      mask.current?.setAttribute("cy", (upY - 5) + 'px');
+      mask.current?.setAttribute("cx", (upX) + 'px');
+    })
+  },[ref,mask])  // trigger before use eff
+ 
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,58 +32,21 @@ export default function Home() {
       </Head>
       
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1>ดีจ้า</h1>
+        <h2>ดีจ้า</h2>
+        <h3>ดีจ้า</h3>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/icon/logo.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+      <div className="pic" ref={ref}>
+        <SvgComponent
+        mask = {mask}
+        />
+      </div>
+
+      <footer>
+
       </footer>
+
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,16 @@ module.exports = {
   },
   plugins: [],
 }
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+      })
+    })
+  ]
+}
